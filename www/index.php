f39abe77f1bd005f23417da10619fd4cc7e731e8
Rich Hildred <rhildred@gmail.com>
2016-03-19 01:20:15 -0400

    made the index and the about page use the layout ... the rest is up to you

---
+++ www/index.php
@@ -0,0 +1,17 @@
+<?php
+require __DIR__ . "/../vendor/autoload.php";
+//connect to database
+$oDb = new PDO("sqlite:" . __DIR__ . "/../gardener.sqlite");
+$oApp = new \Slim\Slim(array(
+        'view' => new \PHPView\PHPView(),
+        'templates.path' => __DIR__ . "/../views" ));
+
+$oApp->get("/", function()use($oApp){
+    $oApp->render("index.phtml");
+});
+
+$oApp->get("/:page", function($sPage)use($oApp){
+    $oApp->render($sPage . ".phtml");
+});
+
+$oApp->run();
