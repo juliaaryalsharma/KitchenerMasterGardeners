1f454442ffca6b9f94fc7afdc78049096757494b
rhildred <rhildred@gmail.com>
2016-04-01 11:53:13 -0400

    added /salt/:uname endpoint

--- www/index.php
+++ www/index.php
@@ -1,7 +1,7 @@
 <?php
 require __DIR__ . "/../vendor/autoload.php";
 //connect to database
-$oDb = new PDO("sqlite:" . __DIR__ . "/../KmgDb.sqlite");
+$oDb = new PDO("sqlite:" . __DIR__ . "/../gardener.sqlite");
 $oApp = new \Slim\Slim(array(
         'view' => new \PHPView\PHPView(),
         'templates.path' => __DIR__ . "/../views" ));
@@ -12,6 +12,14 @@ $oApp->get("/", function()use($oApp){
 
 $oApp->get("/:page", function($sPage)use($oApp){
     $oApp->render($sPage . ".phtml");
+});
+
+$oApp->get("/salt/:uname", function($sUname)use($oApp, $oDb){
+    $oStmt = $oDb->prepare("SELECT salt FROM users WHERE uname = :uname");
+    $oStmt->bindParam("uname", $sUname);
+    $oStmt->execute();
+    $aUser = $oStmt->fetchAll(PDO::FETCH_OBJ);
+    echo json_encode($aUser[0]);
 });
 
 $oApp->run();
